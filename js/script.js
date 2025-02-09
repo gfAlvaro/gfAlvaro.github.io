bf1d673fbd1367f1a8d00695af08fa46a131442b
Álvaro García Fuentes <gf_alvaro@hotmail.com>
2025-02-09 05:12:31 +0100

    adaptado slide para móviles

--- js/script.js
+++ js/script.js
@@ -1,25 +1,23 @@
+/** 
+ * Comportamiento de mi portfolio
+ * @author Álvaro García Fuentes
+ **/
 let slideIndex = 1;
 let plusDivs = n => showDivs(slideIndex += n);
-let x = document.getElementsByClassName("mySlides");
 let showDivs = n => {
-
-    if (n > x.length)
-        slideIndex = 1;
-    if (n < 1)
-        slideIndex = x.length;
-    
-    let isMobile = screen.width < 801;
-
-    for (element of x)
-        element.style.display = (isMobile? "block" : "none");
-
-    if( !isMobile )
-        x[slideIndex-1].style.display = "block";        
+    let misSlides = document.getElementsByClassName("mySlides");
+    if (n > misSlides.length)  slideIndex = 1;
+    if (n < 1)  slideIndex = misSlides.length;
+    for (element of misSlides)
+        element.style.display = element == misSlides[slideIndex-1]? "block" : "none";
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-      showDivs(slideIndex);
-      document.getElementById("menosuno").addEventListener("click", () => plusDivs(-1));
-      document.getElementById("masuno").addEventListener("click", () => plusDivs(1));
- 
+    showDivs(slideIndex);
+    document.getElementById("menosuno").addEventListener("click", () => plusDivs(-1));
+    document.getElementById("masuno").addEventListener("click", () => plusDivs(1)); 
+    document.querySelectorAll(".navoption").forEach(element => element.addEventListener("click", function(){
+        slideIndex = Number(this.getAttribute( "id"));
+        plusDivs(0);
+    }));
 });
