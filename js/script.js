11953986b1b1e63c55485f020d60c1763e84087b
Álvaro García Fuentes <gf_alvaro@hotmail.com>
2026-06-10 17:13:31 +0200

    refactorización para estilos

--- js/script.js
+++ js/script.js
@@ -2,20 +2,23 @@
  * Comportamiento de mi portfolio
  * @author Álvaro García Fuentes
  **/
-let slideIndex = 1;
-let plusDivs = n => showDivs(slideIndex += n);
-let showDivs = n => {
-    let misSlides = document.getElementsByClassName("mySlides");
-    if (n > misSlides.length)  slideIndex = 1;
-    if (n < 1)  slideIndex = misSlides.length;
-    for (element of misSlides)
-        element.style.display = element == misSlides[slideIndex-1]? "block" : "none";
-}
+
 
 document.addEventListener("DOMContentLoaded", () => {
+
+    let slideIndex = 1;
+    let plusDivs = n => showDivs(slideIndex += n);
+    let showDivs = n => {
+        let misSlides = document.getElementsByClassName("mySlides");
+        if (n > misSlides.length)  slideIndex = 1;
+        if (n < 1)  slideIndex = misSlides.length;
+        for (element of misSlides)
+            element.style.display = element == misSlides[slideIndex-1]? "block" : "none";
+    }
+
     showDivs(slideIndex);
-    document.getElementById("menosuno").addEventListener("click", () => plusDivs(-1));
-    document.getElementById("masuno").addEventListener("click", () => plusDivs(1)); 
+    document.querySelector("menosuno").addEventListener("click", () => plusDivs(-1));
+    document.querySelector("masuno").addEventListener("click", () => plusDivs(1)); 
     document.querySelectorAll(".navoption").forEach(element => element.addEventListener("click", function(){
         slideIndex = Number(this.getAttribute( "id"));
         plusDivs(0);
